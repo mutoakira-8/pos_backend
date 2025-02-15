31a98c4bf594bc47546074e80201d81981f8b835
akiramuto1124 <akira.muto1124@gmail.com>
2025-02-15 21:17:49 +0900

    8th commit

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ app = FastAPI()
 # CORS設定
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://tech0-gen8-step4-pos-app-9.azurewebsites.net/"],
+    allow_origins=["https://tech0-gen8-step4-pos-app-9.azurewebsites.net"],
     allow_credentials=True,
     allow_methods=["GET", "POST"],
     allow_headers=["*"],
